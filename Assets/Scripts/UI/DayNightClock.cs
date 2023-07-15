c61e4786b9c8ef1d3cbaef69f07669e1eed41ab9
지호 박 <andrewpark421@gmail.com>
2023-07-15 23:00:51 +0900

    Added day night clock

---
+++ Assets/Scripts/UI/DayNightClock.cs
@@ -0,0 +1,37 @@
+using ScriptableObjectVariable;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightClock : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _sprites;
+    [SerializeField] private int _currentSpriteIndex = 0;
+
+    [Header("Day Night Cycle")]
+    [SerializeField] private BoolReference _isDay;
+    [SerializeField] private FloatReference _dayNightStart;
+    [SerializeField] private FloatReference _dayNightLength;
+
+    private Image _image;
+
+    public float UnitTime { get { return _dayNightLength / (float)_sprites.Count; } }
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float timeFromDayNightStart = Time.time - _dayNightStart;
+        Debug.Log(_currentSpriteIndex * UnitTime);
+        if (timeFromDayNightStart > (_currentSpriteIndex + 1) * UnitTime)
+        {
+            _currentSpriteIndex = (_currentSpriteIndex + 1) % _sprites.Count;
+            _image.sprite = _sprites[_currentSpriteIndex];
+        }
+    }
+}
