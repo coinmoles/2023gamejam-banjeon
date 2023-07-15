ff4fa1eb67cd4ae20b95cb78830ad9c384731081
unknown <andrewpark421@gmail.com>
2023-07-15 19:01:42 +0900

    Changed manager slightly

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    public void On
+}
