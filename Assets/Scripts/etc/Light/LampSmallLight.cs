15622a5c138bfda38888d0beef749fb704d6d977
unknown <yangd1009@naver.com>
2023-07-16 13:22:12 +0900

    light added

---
+++ Assets/Scripts/etc/Light/LampSmallLight.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampSmallLight : MonoBehaviour
+{
+    UnityEngine.Rendering.Universal.Light2D light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DayLight()
+    {
+        light.intensity = 0f;
+        StopAllCoroutines();
+    }
+
+    public void NightLight()
+    {
+        light.intensity = 1f;
+        StartCoroutine(flicking());
+    }
+
+    private IEnumerator flicking()
+    {
+        while (true)
+        {
+            yield return null;
+            light.intensity = 0.5f + Mathf.PingPong(Time.time, 2.5f);
+        }
+    }
+}
